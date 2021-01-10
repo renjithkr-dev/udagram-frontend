f11198f13eac2b85b27a8bffd23b480dbaad0921
Renjith <renjithtech6@gmail.com>
2021-01-10 09:59:27 -0500

    hardcoded cluster ip address for apiHost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
  // apiHost: 'http://localhost:8080/api/v0'
-  apiHost: process.env.API_HOST+'/api/v0'
+  apiHost: 'http://172.23.191.252:30103/api/v0'
 };
 
 /*
